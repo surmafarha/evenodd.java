c1a6cb14e71077d2662cd190a5ab4924be589270
Farhat surma <91253088+surmafarha@users.noreply.github.com>
2021-09-27 21:18:06 +0530

    Create SumofNnaturalnumber.java

---
+++ SumofNnaturalnumber.java
@@ -0,0 +1,9 @@
+public class SumofNnaturalnumbers {
+    public static void main(String[] args) {
+        int i , num = 10 , sum = 0;
+        for(i=1 ; i<=num ; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
