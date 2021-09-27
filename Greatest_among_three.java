7c0bc7f131266ab8f25bb6896d586d92d0ea4967
Farhat surma <91253088+surmafarha@users.noreply.github.com>
2021-09-27 21:15:52 +0530

    Create Greatest_among_three.java

---
+++ Greatest_among_three.java
@@ -0,0 +1,23 @@
+public class Greatest_among_three {
+
+    public static void main(String[] args) {
+
+        int  a=10 , b=100, c=50;
+
+        if (a>b && a<c) {
+
+            System.out.println("A is greater");
+
+        } else if (b>a && b>c) {
+
+            System.out.println("B is greater");
+
+        } else {
+
+            System.out.println("C is greater");
+
+        }
+
+    }
+
+}
