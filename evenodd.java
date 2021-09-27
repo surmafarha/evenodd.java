f29590a5433f77a59f96e3dd3d994ec2c41d201d
Farhat surma <91253088+surmafarha@users.noreply.github.com>
2021-09-27 21:12:41 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+public class EvenOdd {
+
+    public static void main (String[] args) {
+
+        int n = 40;
+
+        if (n%2 == 0) {
+
+            System.out.println("It is even");
+
+        }
+
+        else{
+
+            System.out.println("It is Odd");
+
+        }
+
+    }
+
+}
