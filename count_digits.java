8351bc00d93d1090bb5d1562a76bc150cf1f5616
Farhat surma <91253088+surmafarha@users.noreply.github.com>
2021-09-27 21:18:52 +0530

    Create count_digits.java

---
+++ count_digits.java
@@ -0,0 +1,11 @@
+public class Count_digits {
+
+    public static void main(String[] args) {
+        int n = 12345, count=0 ;
+        for (int i=n ; i>0 . i=i/10 )
+        {
+            count++;
+        }
+        System.out.println("Count is " +count);
+    }
+}
